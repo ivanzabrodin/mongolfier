0380ad66f193cbf56f0048f522cb2ea343b2d2cf
Иван Забродин <zabrodinivan77@gmail.com>
2015-11-26 16:03:14 +0300

    stylized end

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ $('.smoothScroll').click(function(event) {
       scrollTop: top
     }, 1000);
     });
+
+$(document).ready(function(){
+	
+	$('.js-tab-link').on('click', tabToggle);
+
+	function tabToggle(e) {
+	e.preventDefault();
+
+	$('.js-tab-link').removeClass('tab-link-active');
+	$(this).addClass('tab-link-active');
+
+	$('.tab-item').removeClass('tab-item-active');
+	$(this.hash).addClass('tab-item-active');
+	}
+
+});
